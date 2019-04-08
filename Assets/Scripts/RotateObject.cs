f28f56d9596c5975cc79b4c77022f405567c7ff9
Krasen Ivanov <krasen007@gmail.com>
2019-04-08 20:53:45 +0300

    Add rotation

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,22 @@
+﻿namespace Assets.Scripts
+{
+    using UnityEngine;
+
+    public class RotateObject : MonoBehaviour
+    {
+        private const int RotateSpeed = 75;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            // Empty
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            // Empty
+            transform.Rotate(Vector3.forward * Time.deltaTime * RotateSpeed);
+        }
+    }
+}
